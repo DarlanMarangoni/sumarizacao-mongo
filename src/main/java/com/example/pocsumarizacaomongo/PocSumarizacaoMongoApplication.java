4da8e459ad26516766f51cc811530bed10be092d
darlan <dmarangoni.dev@gmail.com>
2025-10-30 19:29:32 -0300

    projeto criado

---
+++ src/main/java/com/example/pocsumarizacaomongo/PocSumarizacaoMongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.pocsumarizacaomongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSumarizacaoMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocSumarizacaoMongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pocsumarizacaomongo/PocSumarizacaoMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pocsumarizacaomongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocSumarizacaoMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
